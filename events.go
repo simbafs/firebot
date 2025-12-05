0bd8820f2e2db42bfc7187ae96d8ef94b1fdf96f
SimbaFs <simba.fs@gmail.com>
2025-12-05 23:54:12 +0800

    chore: rename some variabbles and seperate file

--- events.go
+++ events.go
@@ -2,61 +2,8 @@ package main
 
 import (
 	"fmt"
-	"slices"
-	"strings"
 	"time"
 )
-
-type List []string
-
-func (d List) String() string {
-	return strings.Join(d, ",")
-}
-
-func (d List) Equal(other List) bool {
-	if len(d) != len(other) {
-		return false
-	}
-	for i, v := range d {
-		if v != other[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func (l List) Diff(other List) string {
-	if l.Equal(other) {
-		return ""
-	}
-
-	addition := []string{}
-	deletion := []string{}
-	// addition
-	for _, v := range other {
-		found := slices.Contains(l, v)
-		if !found {
-			addition = append(addition, v)
-		}
-	}
-	// deletion
-	for _, v := range l {
-		found := slices.Contains(other, v)
-		if !found {
-			deletion = append(deletion, v)
-		}
-	}
-
-	s := ""
-	if len(addition) > 0 {
-		s += fmt.Sprintf("新增: %s\n", strings.Join(addition, ","))
-	}
-	if len(deletion) > 0 {
-		s += fmt.Sprintf("刪除: %s\n", strings.Join(deletion, ","))
-	}
-
-	return s
-}
 
 type Event struct {
 	ID       string
